33a62b18ee07408caa17741878787da6346a8a32
Muhammad Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2024-10-26 00:34:04 +0500

    batman

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,27 @@
+const {Router} = require('express')
+const router = Router()
+const {Auth,verifyUser} = require('../middleware/auth')
+
+const {register, login,getUser,updateUser, logout,forgetPassword,resetPassword} = require('../controller/userController')
+ 
+
+router.post('/register',register)
+router.post('/login',login)
+// router.get('/:username',getUser)
+
+router.get('/',Auth,getUser)
+
+// router.get('/createSession',createSession)
+
+
+// router.put('/resetpassword',verifyUser,resetPassword)
+router.put('/',Auth,updateUser)
+router.get('/logout',Auth,logout)
+
+router.post('/forget-password',forgetPassword)
+router.put('/reset-password/:resetToken',resetPassword)
+
+
+
+
+module.exports = router
